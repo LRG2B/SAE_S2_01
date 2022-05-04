29180872f786051ac14a81222c21391aed0ef33d
Reumix <rmchab63@gmail.com>
2022-05-04 09:49:25 +0200

    Update Traitement.h

--- SAE_S2.01/Traitement.h
+++ SAE_S2.01/Traitement.h
@@ -10,10 +10,11 @@
 
 float somme_deltaT(Arbre ab);
 
+float sommeTemp(Arbre ab);
+
 float moy_temp_station(Arbre abre);
 
-
-float moyMois(float mois, ListeStation list);
+float moyTemp(Arbre ab);
 
 
 std::array<std::array<std::array<float, 12>, 18>, 30> tabTempMoyMoisStationAnnee(ListeStation list);
