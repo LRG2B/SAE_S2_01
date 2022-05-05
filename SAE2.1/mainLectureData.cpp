c9198ed2d76d62a95ff98432fb235908f3cfb446
Reumix <rmchab63@gmail.com>
2022-05-05 14:13:59 +0200

    Update mainLectureData.cpp

--- SAE2.1/mainLectureData.cpp
+++ SAE2.1/mainLectureData.cpp
@@ -18,102 +18,89 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
 	SAE_Datas_Heat myStations;
-	//std::cout << myStations.nbMonths() << "\n";
 	ListeStation lB = myStations.getListeStation();
-	//lB.affiche_liste();
-
-	std::vector<std::string> nameMonth = { "Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet",
-	"Aout", "Septembre", "Octobre", "Novemebre", "Decembre" };
-
-    
 
     CSize resolution{ 1920, 1080 };
-    CPoint pos_txt{ 0, 0 };
 
     //Définition des couleurs
     auto noir = MakeARGB(255, 0, 0, 0);
     auto blanc = MakeARGB(255, 255, 255, 255);
+    auto rouge = MakeARGB(255, 155, 0, 0);
+    auto vert = MakeARGB(255, 54, 225, 7);
 
-    const int inter_annee = 58;
-    const int inter_temp = 100;
+    //Définition des intervalles du graphique
+    const int inter_annee = 58;                 //Années
+    const int inter_temp = 100;                 //Températures
 
-    float val_temp;
-    int annee = 1992;
+    float val_temp;                             //Valeur de la température pour une année donnée
+    int annee = 1992;                           //Définition de l'année de début
+    float moyenne_temp = 0;                     //Moyenne de température totale
 
-    CSize taille_point{ 20,20 };
-    CPoint pos_point;
-    CRectangle point;
+    CSize taille_point{ 20,20 };                //Définition de la taille du point du graphique
+    CPoint pos_point;                           //Position du point dans le graphique
+    CPoint pos_point_pre;                       //Position du point précédents dans le graphique
+    CRectangle point;                           //Création du rectangle englobant du point
+    CPoint centre_point{ 10,10 };               //Définition du centre du point
 
-
-	//Récupération de l'objet principal de LibGraph 2
 	ILibGraph2* libgraph = GetLibGraph2();
-	//Affiche la fenêtre graphique avec une taille_rec par défaut
 	libgraph->show(resolution, true);
-	evt e;  //Evénement LibGraph 2
+	evt e;
 
-    //Boucle principale d'événements
     while (libgraph->waitForEvent(e))
     {
         switch (e.type)
         {
         case evt_type::evtRefresh:
-            libgraph->beginPaint();
-            //------------------------Affichage-du-cadre
-            libgraph->setPen(noir, 4.0f, LibGraph2::pen_DashStyles::Solid);
-            libgraph->drawLine(CPoint{ 100,100 }, CPoint{ 100,1000 });              //Cadre gauche
-            libgraph->drawLine(CPoint{ 100,1000 }, CPoint{ 1800,1000 });            //Cadre bas
-            //------------------------Affichage-des-flèches
-            libgraph->drawLine(CPoint{ 100,100 }, CPoint{ 75,125 });                //Flèche haut-gauche
-            libgraph->drawLine(CPoint{ 100,100 }, CPoint{ 125,125 });               //Flèche haut-droite
-            libgraph->drawLine(CPoint{ 1800,1000 }, CPoint{ 1775, 1025 });          //Flèche droite-bas
-            libgraph->drawLine(CPoint{ 1800,1000 }, CPoint{ 1775, 975 });           //Flèche droite-haut
-
-            //libgraph->setFont("Segoe UI", 40, font_styles::FontStyleRegular);
-            //libgraph->setSolidBrush(noir);
-            //libgraph->drawString(CString{"OUI"}, pos_txt);
-
-            //------------------------Affichage-des-intervalles
-            for (float i = 1; i <= 8; i++)
-            {
-                libgraph->drawLine(CPoint{ 90 ,1000 - (inter_temp * i) }, CPoint{ 110, 1000 - (inter_temp * i) });
-            }
-            for (float i = 1; i <= 29; i++)
-            {
-                libgraph->drawLine(CPoint{ 100 + (inter_annee * i), 990 }, CPoint{ 100 + (inter_annee * i), 1010 });
-                val_temp = lB.get_temp_annee(annee);
-                pos_point = CPoint{ 100 + (inter_annee * i) , (val_temp*30) };
-                point = CRectangle{ pos_point, taille_point };
-                libgraph->drawEllipse(point);
-                annee++;
-            }
-
-
-            //30px / deg
-            //1992 -> 2021
-
-            //libgraph->drawEllipse(point);
-            ////point = { CPoint{300,400}, taille_point };
-            ////libgraph->drawEllipse(point);
-            ////libgraph->setPen(noir, 2.0f, LibGraph2::pen_DashStyles::Solid);
-            //libgraph->drawLine(pos_point, CPoint{ 300,400 });
-
-            //Affichage du texte de fermeture
-            /*libgraph->setFont("Consolas" , 20 , font_styles::FontStyleRegular);
-            libgraph->setSolidBrush(noir);
-            libgraph->drawString(txt_quit, pos_txt);*/
-
-            libgraph->endPaint();
-            break;
 
         case evt_type::evtKeyDown:
             switch (e.vkKeyCode)
             {
+            case 'A':
+            {
+                libgraph->beginPaint();
+                //------------------------Affichage-du-cadre
+                libgraph->setPen(noir, 4.0f, LibGraph2::pen_DashStyles::Solid);
+                libgraph->drawLine(CPoint{ 100,100 }, CPoint{ 100,1000 });              //Cadre gauche
+                libgraph->drawLine(CPoint{ 100,1000 }, CPoint{ 1800,1000 });            //Cadre bas
+                //------------------------Affichage-des-flèches
+                libgraph->drawLine(CPoint{ 100,100 }, CPoint{ 75,125 });                //Flèche haut-gauche
+                libgraph->drawLine(CPoint{ 100,100 }, CPoint{ 125,125 });               //Flèche haut-droite
+                libgraph->drawLine(CPoint{ 1800,1000 }, CPoint{ 1775, 1025 });          //Flèche droite-bas
+                libgraph->drawLine(CPoint{ 1800,1000 }, CPoint{ 1775, 975 });           //Flèche droite-haut
+
+                //------------------------Affichage-des-intervalles-/-valeurs
+                for (float i = 1; i <= 8; i++)
+                {
+                    libgraph->drawLine(CPoint{ 90 ,1000 - (inter_temp * i) }, CPoint{ 110, 1000 - (inter_temp * i) });          // Affichage des intervalles sur l'axe des ordonnées
+                }
+                pos_point_pre = CPoint{ 100 + (inter_annee * 1) , (1000 - (11.8620358 - 11) * 300) };
+                for (float i = 1; i <= 29; i++)
+                {
+                    libgraph->setPen(noir, 4.0f, LibGraph2::pen_DashStyles::Solid);
+                    libgraph->drawLine(CPoint{ 100 + (inter_annee * i), 990 }, CPoint{ 100 + (inter_annee * i), 1010 });        // Affichage des intervalles sur l'axe des abscisses
+                    val_temp = lB.get_temp_annee(annee);
+                    pos_point = CPoint{ 100 + (inter_annee * i) , (1000-(val_temp-11)*300) };                                   //Redéfinition de la position du point en fonction de la valeur de la température et de l'année
+                    point = CRectangle{ pos_point, taille_point };                                                              //Redéfinition du rectangle englobant (modification de sa position)
+                    libgraph->drawEllipse(point);                                                                               //Affichage du point
+                    libgraph->setPen(rouge, 4.0f, LibGraph2::pen_DashStyles::Solid);
+                    libgraph->drawLine(pos_point+centre_point, pos_point_pre+centre_point);                                     //Affichage d'un trait reliant le point actuel et la point précédent
+                    moyenne_temp += val_temp;                                                                                   //Incrémentation de la valeur de la température actuel
+                    annee++;                                                                                                    //Incrémentation de l'année
+                    pos_point_pre = pos_point;                                                                                  //Redéfinition de la position du point précédent au point actuel
+                }
+
+                moyenne_temp /= 29;                                                                                             //Calcule de la valeur moyenne de la température sur les 30 années
+                libgraph->setPen(vert, 5.0f, LibGraph2::pen_DashStyles::Dash);
+                libgraph->drawLine(CPoint{ 158, 1000 - (moyenne_temp-11)*300 }, CPoint{ 1800,1000-(moyenne_temp-11)*300 });     //Affichage d'un trait correspondant à la moyenne de température
+
+                libgraph->endPaint();
+                break;
+            }
             case VK_ESCAPE:
                 exit(0);
                 break;
             }
             break;
-
         case evt_type::evtMouseMove:
             break;
         }
